809ab467a8ef8f51fb897e742953fdb022b25407
Julia <barkovaulia582@gmail.com>
2025-11-15 13:00:48 +0300

    second commit

--- src/main/java/com/barkova/program/Main.java
+++ src/main/java/com/barkova/program/Main.java
@@ -3,5 +3,7 @@ package com.barkova.program;
 public class Main {
     public static void main(String[] args) {
         System.out.println("ggggg");
+        System.out.println("second commit");
+
     }
 }
